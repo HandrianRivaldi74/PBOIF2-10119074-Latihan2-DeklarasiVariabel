7cf821325b2ec14a96b830743f127e7cf8fc2f3d
HandrianRivaldi74 <handrianr28@gmail.com>
2020-10-18 13:12:09 +0700

    Latihan2

---
+++ src/pboif2_10119074_latihan2/PBOIF2_10119074_Latihan2.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2_10119074_latihan2;
+
+/**
+ *
+ * @author Acer
+ * NAMA     : Handrian Rivaldi
+ * KELAS    : PBOIF2
+ * NIM      : 10119074
+ * Deskrifsi : Bentuk umum pendeklarasisan variabel dalam java
+ */
+public class PBOIF2_10119074_Latihan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Deklarasi variabel
+        int nilaiInt;
+        final double PHI = 3.14; //konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //Memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        //Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai      = " + nilaiInt);
+        System.out.println("Isi variabel PHI        = " + PHI);
+        System.out.println("Isi variabel logika     = "  + nilaiLogika);
+        System.out.println("Isi variabel Karakter   = " + nilaiKarakter);
+    
+    }
+    
+}
